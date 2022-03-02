996488f16dadbd6fabaa5904b3f5c5c37f800512
AbnerJosuEstrada <josueestrada@twowheelstogo.com>
2022-03-02 10:15:46 -0600

    new changes

--- src/index.js
+++ src/index.js
@@ -1,26 +1,14 @@
-import startup from "./startup.js";
+import pkg from "../package.json";
 
 /**
  * @summary Import and call this function to add this plugin to your API.
- * @param {ReactionAPI} app ReactionAPI instance
+ * @param {ReactionAPI} app The ReactionAPI instance
  * @returns {undefined}
  */
 export default async function register(app) {
-    /**
-     * Simple Ping Pong like example plugin
-     * Allows the user to query HybrIT and return the slogan with desired language
-     */
-    await app.registerPlugin({
-      label: "HybrIT Plugin Template",
-      name: "reaction-plugin-template",
-      functionsByType: {
-          startup: [startup]
-      },
-      collections: {
-        // Make sure that the object key name is the same as the name value.
-        ExampleTemplateCollection: {
-              name: "ExampleTemplateCollection"
-          }
-      }
-    });
-  }
+  await app.registerPlugin({
+    label: "Plugin Example",
+    name: "plugin-example",
+    version: pkg.version
+  });
+}
